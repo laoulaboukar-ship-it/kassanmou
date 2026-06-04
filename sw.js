81ab98735cb06232862b01030abae9a43d3cb232
laoulaboukar-ship-it <laoulaboukar@gmail.com>
2026-06-04 12:11:53 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Kassan'Mou SW v60 — www.kassanmou.net */
-var CACHE = 'km-v62i';
+var CACHE = 'km-v62j';
 var PRECACHE = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', function(e) {
